bc04a71867cf10389610590545beb1030a3f2e70
eatmyvenom <eat.my.venomm@gmail.com>
2023-01-19 21:39:12 -0800

    fix(vnmm.dev): dir

--- domains/vnmm.dev/app/share/page.tsx
+++ domains/vnmm.dev/app/share/page.tsx
@@ -1,4 +1,5 @@
 import { readdirSync } from "fs";
+import {join} from "path";
 
 const shareDir = process.env.SHARE_DIR ?? "public/";
 
@@ -7,7 +8,7 @@ export default function page() {
 
   return <div style={{padding: "18px"}}>
     {ls.map(path => {
-      return <><a style={{color: "#44a3e7"}} key={path} href={path}> {path.split("/").at(-1)} </a><br /></>
+      return <><a style={{color: "#44a3e7"}} key={path} href={join(shareDir, path)}> {path.split("/").at(-1)} </a><br /></>
     })}
   </div>
 }
